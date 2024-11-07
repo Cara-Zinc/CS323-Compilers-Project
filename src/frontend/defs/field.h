470ebc551a136c0ebe96ac70fe6fe6e899542122
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-07 23:37:13 +0800

    added new functions to field and func

--- src/frontend/defs/field.h
+++ src/frontend/defs/field.h
@@ -3,6 +3,7 @@
 
 #include "list.h"
 #include "type.h"
+#include "util.h"
 #include <stdbool.h>
 
 typedef struct {
@@ -13,6 +14,16 @@ typedef struct {
     size_t array_size;
 } field_def;
 
+field_def *field_def_new(char *name, type_id type) {
+    field_def *res = new(field_def);
+    res->name = name;
+    res->type = type;
+    res->is_struct = false;
+    res->is_array = false;
+    res->array_size = 0;
+    return res;
+}
+
 typedef struct {
     char *name;
     type_id type;
@@ -21,5 +32,16 @@ typedef struct {
     size_t array_size;
     void *value;
 } readonly_field_def;
+
+readonly_field_def *readonly_field_def_new(char *name, type_id type, void *value) {
+    readonly_field_def *res = new(readonly_field_def);
+    res->name = name;
+    res->type = type;
+    res->is_struct = false;
+    res->is_array = false;
+    res->array_size = 0;
+    res->value = value;
+    return res;
+}
 
 #endif
--- src/frontend/defs/func.h
+++ src/frontend/defs/func.h
@@ -3,11 +3,21 @@
 
 #include "list.h"
 #include "type.h"
+#include "util.h"
+#include "field.h"
 
 typedef struct {
     char *name;
     type_id return_type;
     list *args;
 } func_def;
+
+func_def *func_def_new(char *name, type_id return_type) {
+    func_def *res = new(func_def);
+    res->name = name;
+    res->return_type = return_type;
+    res->args = list_new(field_def);
+    return res;
+}
 
 #endif
