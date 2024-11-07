b200e0eeecd32cb9e8e61da29139782a0e7662cc
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-08 00:16:55 +0800

    added type_spec_def

--- src/frontend/defs/type.h
+++ src/frontend/defs/type.h
@@ -2,15 +2,36 @@
 #define __TYPE_H__
 
 #include <stddef.h>
+#include <stdbool.h>
+#include "util.h"
 
 typedef size_t type_id;
 
-// variable type is design as follows and convert them into type_id
-// 0: int
-// 1: float
-// 2: char
-#define TYPE_INT 0
-#define TYPE_FLOAT 1
-#define TYPE_CHAR 2
+// types of values are designed as follows
+#define TYPE_VOID 0 // void type
+#define TYPE_INT 1 // integer type
+#define TYPE_FLOAT 2 // floating point type
+#define TYPE_CHAR 3 // character type
+
+// specification for type
+typedef struct {
+    type_id type_id; // type id of the type
+    bool is_struct;
+    bool is_array;
+    size_t array_size;
+} type_spec_def;
+
+type_spec_def *type_spec_def_new(type_id type_id, bool is_struct) {
+    type_spec_def *res = new(type_spec_def);
+    res->type_id = type_id;
+    res->is_struct = is_struct;
+    res->is_array = false;
+    res->array_size = 0;
+    return res;
+}
+
+void type_spec_def_free(type_spec_def *t) {
+    free(t);
+}
 
 #endif
