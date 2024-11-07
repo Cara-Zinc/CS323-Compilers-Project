aac5d68a1c8eb82ef9d2d1e7983426f8f8020195
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-08 00:31:40 +0800

    doc for type

--- src/frontend/defs/type.h
+++ src/frontend/defs/type.h
@@ -7,7 +7,7 @@
 
 typedef size_t type_id;
 
-// types of values are designed as follows
+// types of values are designed as follows when type_spec_def::is_struct is false:
 #define TYPE_VOID 0 // void type
 #define TYPE_INT 1 // integer type
 #define TYPE_FLOAT 2 // floating point type
@@ -16,11 +16,12 @@ typedef size_t type_id;
 // specification for type
 typedef struct {
     type_id type_id; // type id of the type
-    bool is_struct;
-    bool is_array;
-    size_t array_size;
+    bool is_struct; // whether the type is a struct
+    bool is_array; // whether the type is an array
+    size_t array_size; // size of the array
 } type_spec_def;
 
+// create a new type specification
 type_spec_def *type_spec_def_new(type_id type_id, bool is_struct) {
     type_spec_def *res = new(type_spec_def);
     res->type_id = type_id;
@@ -30,6 +31,7 @@ type_spec_def *type_spec_def_new(type_id type_id, bool is_struct) {
     return res;
 }
 
+// free a type specification
 void type_spec_def_free(type_spec_def *t) {
     free(t);
 }
