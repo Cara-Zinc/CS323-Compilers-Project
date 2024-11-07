141dfe3d49efbaae828fbdf872c0530e56dc741e
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-07 21:51:28 +0800

    reformated field & type

--- src/frontend/defs/field.h
+++ src/frontend/defs/field.h
@@ -3,16 +3,22 @@
 
 #include "list.h"
 #include "type.h"
+#include <stdbool.h>
 
 typedef struct {
     char *name;
     type_id type;
-
+    bool is_struct;
+    bool is_array;
+    size_t array_size;
 } field_def;
 
 typedef struct {
     char *name;
     type_id type;
+    bool is_struct;
+    bool is_array;
+    size_t array_size;
     void *value;
 } readonly_field_def;
 
--- src/frontend/defs/type.h
+++ src/frontend/defs/type.h
@@ -3,17 +3,12 @@
 
 #include <stddef.h>
 
-#define TYPE_ARRAY_FLAG (1 << 24)
-#define TYPE_STRUCT_FLAG (1 << 25)
-
 typedef size_t type_id;
 
-// variable type is design as follows and convert them into type_id, bit 24 and bit 25 are ARRAY_FLAG and STRUCT_FLAG:
+// variable type is design as follows and convert them into type_id
 // 0: int
 // 1: float
 // 2: char
-// 1<<24: array flag, maximum number of arrays is 2^24
-// 1<<25: struct flag, maximum number of structs is 2^24
 #define TYPE_INT 0
 #define TYPE_FLOAT 1
 #define TYPE_CHAR 2
