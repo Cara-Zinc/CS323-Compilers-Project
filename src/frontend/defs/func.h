55b5dda7d3b436938a0145091114b7c8fb4f8a1e
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-08 00:36:55 +0800

    let func_def returns use type_spec_def, doc for func

--- src/frontend/defs/func.h
+++ src/frontend/defs/func.h
@@ -6,22 +6,26 @@
 #include "util.h"
 #include "field.h"
 
+// definition of a function
 typedef struct {
-    char *name;
-    type_id return_type;
-    list *args;
+    char *name; // name of the function
+    type_spec_def *return_type_spec; // return type specification of the function
+    list *args; // arguments of the function
 } func_def;
 
-func_def *func_def_new(char *name, type_id return_type) {
+// create a new function definition
+func_def *func_def_new(char *name, type_spec_def *return_type_spec) {
     func_def *res = new(func_def);
     res->name = name;
-    res->return_type = return_type;
+    res->return_type_spec = return_type_spec;
     res->args = list_new(field_def);
     return res;
 }
 
+// free a function definition
 void func_def_free(func_def *f) {
     free(f->name);
+    type_spec_def_free(f->return_type_spec);
     list_free(f->args);
     free(f);
 }
