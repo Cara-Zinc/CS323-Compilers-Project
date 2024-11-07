111462a1bd4e705768bab1399b41652085834fd5
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-08 00:32:49 +0800

    doc for struct

--- src/frontend/defs/struct.h
+++ src/frontend/defs/struct.h
@@ -6,12 +6,14 @@
 #include "field.h"
 #include "func.h"
 
+// definition of a struct
 typedef struct {
-    char *name;
-    list *fields;
-    list *funcs;
+    char *name; // name of the struct
+    list *fields; // fields of the struct
+    list *funcs; // functions of the struct
 } struct_def;
 
+// create a new struct definition
 struct_def *struct_def_new(char *name) {
     struct_def *s = new(struct_def);
     s->name = name;
@@ -19,6 +21,7 @@ struct_def *struct_def_new(char *name) {
     s->funcs = list_new(func_def);
 }
 
+// free a struct definition
 void struct_def_free(struct_def *s) {
     free(s->name);
     list_free(s->fields);
@@ -26,5 +29,5 @@ void struct_def_free(struct_def *s) {
     free(s);
 }
 
-// Let's call it Structra! 
+// Let's call it Structra! LGTM!
 #endif
