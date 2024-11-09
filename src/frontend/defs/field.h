f87b2fe381d0c0cc37f6414676dad771ebe6a48f
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-10 02:00:38 +0800

    added varlist

--- src/frontend/defs/field.h
+++ src/frontend/defs/field.h
@@ -33,15 +33,6 @@ void field_def_free(field_def *f) {
 #define V field_def*
 #include <cmc/hashmap.h>
 
-struct varmap_fkey vmap_fkeys = {
-    .cmp = cmc_str_cmp,
-    .cpy = cmc_str_cpy,
-    .str = cmc_str_str,
-    .free = free,
-    .hash = cmc_str_hash_java,
-    .pri = cmc_str_cmp,
-};
-
 int field_def_cmp(field_def *f1, field_def *f2) {
     return cmc_chr_cmp(f1->name, f2->name);
 }
@@ -60,7 +51,30 @@ size_t field_def_hash(field_def *f) {
     return cmc_str_hash_java(f->name);
 }
 
+struct varmap_fkey vmap_fkeys = {
+    .cmp = cmc_str_cmp,
+    .cpy = cmc_str_cpy,
+    .str = cmc_str_str,
+    .free = free,
+    .hash = cmc_str_hash_java,
+    .pri = cmc_str_cmp,
+};
+
 struct varmap_fval vmap_fvals = {
+    .cmp = field_def_cmp,
+    .cpy = field_def_cpy,
+    .str = field_def_str,
+    .free = field_def_free,
+    .hash = field_def_hash,
+    .pri = field_def_cmp,
+};
+
+#define SNAME varlist
+#define PFX vlist
+#define V field_def*
+#include <cmc/list.h>
+
+struct varlist_fval vlist_fvals = {
     .cmp = field_def_cmp,
     .cpy = field_def_cpy,
     .str = field_def_str,
