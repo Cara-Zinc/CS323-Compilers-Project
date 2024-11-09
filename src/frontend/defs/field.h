71b4109c2cd77ce3c25ae15ef61199898f95fd7d
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-09 22:17:41 +0800

    added varmap

--- src/frontend/defs/field.h
+++ src/frontend/defs/field.h
@@ -25,4 +25,48 @@ void field_def_free(field_def *f) {
     free(f);
 }
 
+#include <cmc/utl/futils.h>
+
+#define SNAME varmap
+#define PFX vmap
+#define K char*
+#define V field_def*
+#include <cmc/hashmap.h>
+
+struct varmap_fkey vmap_fkeys = {
+    .cmp = cmc_str_cmp,
+    .cpy = cmc_str_cpy,
+    .str = cmc_str_str,
+    .free = free,
+    .hash = cmc_str_hash_java,
+    .pri = cmc_str_cmp,
+};
+
+int field_def_cmp(field_def *f1, field_def *f2) {
+    return cmc_chr_cmp(f1->name, f2->name);
+}
+
+field_def *field_def_cpy(field_def *f) {
+    char *name = str_copy(f->name);
+    return field_def_new(name, type_def_cpy(f->type_spec));
+}
+
+bool field_def_str(FILE *file, field_def *f) {
+    fprintf(file, "variable %s", f->name);
+    return true;
+}
+
+size_t field_def_hash(field_def *f) {
+    return cmc_str_hash_java(f->name);
+}
+
+struct varmap_fval vmap_fvals = {
+    .cmp = field_def_cmp,
+    .cpy = field_def_cpy,
+    .str = field_def_str,
+    .free = field_def_free,
+    .hash = field_def_hash,
+    .pri = field_def_cmp,
+};
+
 #endif
