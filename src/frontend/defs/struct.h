fcdf17670db34158913af90a4533c7b0b935c3f3
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2024-11-10 15:59:57 +0800

    moved fields and funcs in struct_def to scope, added structmap

--- src/frontend/defs/struct.h
+++ src/frontend/defs/struct.h
@@ -1,33 +1,75 @@
 #ifndef __STRUCT_H__
 #define __STRUCT_H__
 
-#include "list.h"
+// Let's call it Structra! LGTM!
+
 #include "util.h"
-#include "field.h"
-#include "func.h"
+#include "scope.h"
 
 // definition of a struct
 typedef struct {
     char *name; // name of the struct
-    list *fields; // fields of the struct
-    list *funcs; // functions of the struct
+    scope *scope; // scope of the struct
 } struct_def;
 
 // create a new struct definition
 struct_def *struct_def_new(char *name) {
     struct_def *s = new(struct_def);
     s->name = name;
-    s->fields = list_new(field_def);
-    s->funcs = list_new(func_def);
+    s->scope = scope_new();
 }
 
 // free a struct definition
 void struct_def_free(struct_def *s) {
     free(s->name);
-    list_free(s->fields);
-    list_free(s->funcs);
+    scope_free(s->scope);
     free(s);
 }
 
-// Let's call it Structra! LGTM!
+#define SNAME structmap
+#define PFX smap
+#define K char*
+#define V struct_def*
+#include <cmc/hashmap.h>
+
+typedef struct structmap structmap;
+
+struct structmap_fkey smap_fkeys = {
+    .cmp = cmc_str_cmp,
+    .cpy = cmc_str_cpy,
+    .str = cmc_str_str,
+    .free = free,
+    .hash = cmc_str_hash_java,
+    .pri = cmc_str_cmp,
+};
+
+int struct_def_cmp(struct_def *s1, struct_def *s2) {
+    return cmc_str_cmp(s1->name, s2->name);
+}
+
+struct_def *struct_def_cpy(struct_def *s) {
+    struct_def *res = new(struct_def);
+    res->name = str_copy(s->name);
+    res->scope = scope_cpy(s->scope);
+    return res;
+}
+
+bool struct_def_str(FILE *file, struct_def *s) {
+    fprintf(file, "struct %s", s->name);
+    return true;
+}
+
+size_t struct_def_hash(struct_def *s) {
+    return cmc_str_hash_java(s->name);
+}
+
+struct structmap_fval smap_fvals = {
+    .cmp = struct_def_cmp,
+    .cpy = struct_def_cpy,
+    .str = struct_def_str,
+    .free = struct_def_free,
+    .hash = struct_def_hash,
+    .pri = struct_def_cmp,
+};
+
 #endif
