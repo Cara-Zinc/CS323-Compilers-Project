b703f443af22003b3e26ff973132598c1939ae7c
Cara-Zinc <zincala1084@gmail.com>
2024-11-18 01:53:23 +0800

    add test for AST printing

---
+++ tests/ast/test_rprint_ast.c
@@ -0,0 +1,27 @@
+#include "../../src/frontend/parser/ast.h"
+
+int main() {
+    // Create leaf nodes (e.g., INT, ID)
+    ASTNode *intTypeNode = createASTLeaf("Type", "int");
+    ASTNode *idNode1 = createASTLeaf("ID", "x");
+    ASTNode *idNode2 = createASTLeaf("ID", "y");
+    ASTNode *intLiteralNode = createASTLeaf("INT", "10");
+
+    // Create nodes representing a declaration (int x;)
+    ASTNode *varDeclNode1 = createASTNode("VarDecl", 2, intTypeNode, idNode1);
+
+    // Create nodes representing another declaration (int y = 10;)
+    ASTNode *varDeclNode2 = createASTNode("VarDecl", 3, intTypeNode, idNode2, intLiteralNode);
+
+    // Create a compound statement node that contains the variable declarations
+    ASTNode *compoundStmtNode = createASTNode("CompoundStmt", 2, varDeclNode1, varDeclNode2);
+
+    // Print the entire AST using rprintAST
+    printf("Printing the AST using rprintAST:\n");
+    rprintAST(compoundStmtNode, 0);
+
+    // Free the entire AST to avoid memory leaks
+    freeAST(compoundStmtNode);
+
+    return 0;
+}
