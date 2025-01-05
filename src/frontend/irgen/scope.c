0f3bc0fd036642946a5e64596e14fcb3a2f86de6
Cara-Zinc <zincala1084@gmail.com>
2025-01-05 07:27:11 +0000

    remove invalid implementation

--- src/frontend/irgen/scope.c
+++ src/frontend/irgen/scope.c
@@ -11,10 +11,6 @@ void scope_ir_gen(scope *s, IRContext *ctx) {
     // generate IR for all fields
 
     // generate IR for all funcs
-    for(int i=0; i<fmap_count(s->funcs); i++) {
-        func_def *f = fmap_get(s->funcs, i);
-        func_ir_gen(f, ctx);
-    }
 
     // generate IR for all subscopes
     for(int i=0; i<sclist_count(s->subscopes); i++) {
