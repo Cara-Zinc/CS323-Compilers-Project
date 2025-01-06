e3d842718c2b508777aa42debdd4aadb2658dbb3
charleyhut <charleyxiao057@gmail.com>
2025-01-06 18:49:27 +0800

    ret

--- src/frontend/irgen/scope.c
+++ src/frontend/irgen/scope.c
@@ -41,6 +41,16 @@ void func_ir_gen(func_def *f, IRContext *ctx)
     }
     // generate IR for all statements
     stmtlist_ir_gen(f->stmts, ctx);
+
+    // return statement
+    if (f->return_type != TYPE_VOID)
+    {
+        ir_context_append(ctx, "ret %s %%%s\n", map_type_to_llvm(f->return_type, ctx->pm), "retval");
+    }
+    // else
+    // {
+    //     ir_context_append(ctx, "ret void\n");
+    // }
     ir_context_append(ctx, "}\n");
 }
 
